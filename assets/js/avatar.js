361d6b4cc720078bc5f3f58b6ba77ba61286ae52
dw0rsec <155551510+dw0rsec@users.noreply.github.com>
2024-12-18 15:48:25 +0100

    bugfix

--- assets/js/avatar.js
+++ assets/js/avatar.js
@@ -2,12 +2,9 @@ const img = document.getElementById("dw0rsec");
 const centerThreshold = 100;
 const imgWidth = img.width;
 const imgHeight = img.height;
-
 const centerX = imgWidth / 2;
 const centerY = imgHeight / 2;
-
 const killer = '%cYOU MURDERER !!!';
-
 
 let clickCount = 1;
 
@@ -85,44 +82,52 @@ function generateRandomString(length) {
 const randomString = generateRandomString(32);
 const secretKey = randomString; // should be on the backend !
 
-img.addEventListener("click", function() {
-  if (clickCount === 1) {
-    console.log('Yikes !');
-  } else if (clickCount === 2) {
-    console.log('Whoa !');
-  } else if (clickCount === 3) {
-    console.log('Agh !');
-  } else if (clickCount === 4) {
-    console.log('Ugh, that hurt !');
-  } else if (clickCount === 5) {
-    console.log('Ugh, why ?!');
-  } else if (clickCount === 6) {
-    console.log('Not again...');
-  } else if (clickCount === 7) {
-    console.log('Seriously ?!');
-  } else if (clickCount === 8) {
-    console.log('That\'s going to leave a mark!');
-  } else if (clickCount === 9) {
-    console.log('I feel that one.');
-  } else if (clickCount === 10) {
-    console.log('Ow! My feelings!');
-  } else if (clickCount === 11) {
-    console.log('Oof... this is too much.');
-  } else if (clickCount === 12) {
-    console.log('Can I just... no?!');
-  } else if (clickCount === 13) {
-    console.log('I give up...');
-  } else if (clickCount === 14) {
-    console.log('Really?!');
-  } else if (clickCount === 15) {
-    console.log('That\'s it, I\'m done!');
-  } else if (clickCount === 16) {
-    console.log(killer, 'color: red');
-    img.src = "/assets/images/tombstone.png";
-    createJWT(payload, secretKey).then(jwt => {
-      setCookie('token', jwt);
-    });
-  } 
+img.addEventListener("click", function(event) {
+  const mouseX = event.offsetX;
+  const mouseY = event.offsetY;
 
-  clickCount++;
-});
+  if (
+    mouseX > centerX - centerThreshold && mouseX < centerX + centerThreshold &&
+    mouseY > centerY - centerThreshold && mouseY < centerY + centerThreshold
+  ) {
+    if (clickCount === 1) {
+      console.log('Yikes !');
+    } else if (clickCount === 2) {
+      console.log('Whoa !');
+    } else if (clickCount === 3) {
+      console.log('Agh !');
+    } else if (clickCount === 4) {
+      console.log('Ugh, that hurt !');
+    } else if (clickCount === 5) {
+      console.log('Ugh, why ?!');
+    } else if (clickCount === 6) {
+      console.log('Not again...');
+    } else if (clickCount === 7) {
+      console.log('Seriously ?!');
+    } else if (clickCount === 8) {
+      console.log('That\'s going to leave a mark!');
+    } else if (clickCount === 9) {
+      console.log('I feel that one.');
+    } else if (clickCount === 10) {
+      console.log('Ow! My feelings!');
+    } else if (clickCount === 11) {
+      console.log('Oof... this is too much.');
+    } else if (clickCount === 12) {
+      console.log('Can I just... no?!');
+    } else if (clickCount === 13) {
+      console.log('I give up...');
+    } else if (clickCount === 14) {
+      console.log('Really?!');
+    } else if (clickCount === 15) {
+      console.log('That\'s it, I\'m done!');
+    } else if (clickCount === 16) {
+      console.log(killer, 'color: red');
+      img.src = "/assets/images/tombstone.png";
+      createJWT(payload, secretKey).then(jwt => {
+        setCookie('token', jwt);
+      });
+    } 
+
+    clickCount++;
+  }
+}); 
